07c432bff80aef2cbc90bc7a88d975f349b8eae0
Cody Bontecou <bontecouc@gmail.com>
2022-01-20 21:51:28 -1000

    enctype in form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -7,6 +7,7 @@ export default function ContactForm() {
         method="POST"
         action="/?success=true"
         data-netlify="true"
+        encType="application/x-www-form-urlencoded"
       >
         <input type="hidden" name="form-name" value="contact" />
 
@@ -15,15 +16,15 @@ export default function ContactForm() {
             <div className="grid grid-cols-3 gap-6">
               <div className="col-span-3 sm:col-span-2">
                 <label
-                  htmlFor="company-email"
+                  htmlFor="email"
                   className="block text-sm font-medium text-gray-700"
                 >
                   Email
                 </label>
                 <div className="mt-1 flex rounded-md shadow-sm border border-gray-300 ">
                   <input
+                    name="email"
                     type="text"
-                    name="company-website"
                     id="company-website"
                     className="focus:ring-gray-500 focus:border-gray-500 flex-1 p-3 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
                     placeholder="Enter your email"
@@ -34,15 +35,15 @@ export default function ContactForm() {
 
             <div>
               <label
-                htmlFor="about"
+                htmlFor="message"
                 className="block text-sm font-medium text-gray-700"
               >
                 Message
               </label>
               <div className="mt-1">
                 <textarea
-                  id="about"
-                  name="about"
+                  id="message"
+                  name="message"
                   rows={3}
                   className="p-3 shadow-sm focus:ring-gray-500 focus:border-gray-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
                   placeholder="Send us a message!"
